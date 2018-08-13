49ca059bf116b9dcc79ef3c89a21d663f41bcd2a
‘Alexis <Espinoza>
2018-08-13 01:40:11 -0400

    First Commit

---
+++ app/app.js
@@ -0,0 +1,44 @@
+var budgetController = (function(){
+
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+})();
+
+var UIController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput:function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,                
+                
+            }
+        }
+    }
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var ctrlAddItem = function() {
+
+        // 1. Get field input 
+    }
+
+})(budgetController, UIController);
